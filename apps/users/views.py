e3f7e4be84aca857348954b84015917479278156
huangguahuai <705141203@qq.com>
2017-11-22 14:11:58 +0800

    打开各个页面的方法

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render, render_to_response, HttpResponse
 
 def index(request):
     return render_to_response('index.html', locals())
+
+def about(request):
+    return render_to_response('CompanyProfile.html', locals())
+
+def news(request):
+    return render_to_response('news.html', locals())
+
+def products(request):
+    return render_to_response('products.html', locals())
+
+def SuccessProducts(request):
+    return render_to_response('SuccessProducts.html', locals())
