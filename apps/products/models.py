3ea567fb887fc149e6bf3112e4f9ae9564a683e5
wl30564 <191801737@qq.com>
2017-12-13 10:38:26 +0800

    '更新产品页面'

--- apps/products/models.py
+++ apps/products/models.py
@@ -10,7 +10,7 @@ class Product(models.Model):
     desc = models.CharField(max_length=300, verbose_name=u'产品描述')
     detail = models.TextField(verbose_name=u'产品详情')
     image = models.ImageField(upload_to='product/%Y/%m', verbose_name=u'封面图片', max_length=100,
-                              default='image/default.png')
+                              default='image/default.png', null=True, blank=True)
     tag = models.CharField(default="", max_length=10, verbose_name="产品标签", null=True, blank=True)
     add_time = models.DateTimeField(default=datetime.datetime.now, verbose_name=u'添加时间')
 
